62c5a6506a6bf6ac474d255abe96091fbfa89221
Jack Saunders <jackqu7@gmail.comm>
2016-01-29 11:15:38 +0000

    add motivational squirrel

--- freight/notifiers/slack.py
+++ freight/notifiers/slack.py
@@ -3,11 +3,29 @@ from __future__ import absolute_import
 __all__ = ['SlackNotifier']
 
 import json
+import random
 
 from freight import http
 from freight.models import App, TaskStatus
 
 from .base import Notifier, NotifierEvent
+
+
+SQUIRRELS = [
+    "http://images.cheezburger.com/completestore/2011/11/2/aa83c0c4-2123-4bd3-8097-966c9461b30c.jpg",
+    "http://images.cheezburger.com/completestore/2011/11/2/46e81db3-bead-4e2e-a157-8edd0339192f.jpg",
+    "http://28.media.tumblr.com/tumblr_lybw63nzPp1r5bvcto1_500.jpg",
+    "http://i.imgur.com/DPVM1.png",
+    "http://d2f8dzk2mhcqts.cloudfront.net/0772_PEW_Roundup/09_Squirrel.jpg",
+    "http://www.cybersalt.org/images/funnypictures/s/supersquirrel.jpg",
+    "http://www.zmescience.com/wp-content/uploads/2010/09/squirrel.jpg",
+    "http://img70.imageshack.us/img70/4853/cutesquirrels27rn9.jpg",
+    "http://img70.imageshack.us/img70/9615/cutesquirrels15ac7.jpg",
+    "https://dl.dropboxusercontent.com/u/602885/github/sniper-squirrel.jpg",
+    "http://1.bp.blogspot.com/_v0neUj-VDa4/TFBEbqFQcII/AAAAAAAAFBU/E8kPNmF1h1E/s640/squirrelbacca-thumb.jpg",
+    "https://dl.dropboxusercontent.com/u/602885/github/soldier-squirrel.jpg",
+    "https://dl.dropboxusercontent.com/u/602885/github/squirrelmobster.jpeg",
+]
 
 
 class SlackNotifier(Notifier):
@@ -43,7 +61,8 @@ class SlackNotifier(Notifier):
         elif task.status == TaskStatus.cancelled:
             title = "[{app_name}/{env}] Deploy <{link}|#{number}> ({sha}) was cancelled after {duration}s".format(**params)
         elif task.status == TaskStatus.finished:
-            title = "[{app_name}/{env}] Successfully deployed <{link}|#{number}> ({sha}) after {duration}s".format(**params)
+            title = "[{app_name}/{env}] Successfully deployed <{link}|#{number}> ({sha}) after {duration}s ".format(**params)
+            title += random.choice(SQUIRRELS)
         else:
             raise NotImplementedError(task.status)
 
